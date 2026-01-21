a1b128d64f39ddcc0d043858696db383c524edc7
Darshan Dhawade <darshandhawade8@gmail.com>
2026-01-21 22:32:56 +0530

    Add files via upload

---
+++ Assignment_09/Assignment9_5.py
@@ -0,0 +1,7 @@
+# Write a program which accepts one number and checks whether it is divisble by 3 and 5
+
+No = 15
+if No % 3 == 0 and  No % 5 == 0 :
+    print("Divisible by 3 and 5")
+else:
+    print("Not Divisible by 3 and 5")
