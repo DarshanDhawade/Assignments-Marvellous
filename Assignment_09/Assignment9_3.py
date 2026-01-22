d6190282d2638b444d1764ef25e47bfee20b5102
Darshan Dhawade <darshandhawade8@gmail.com>
2026-01-22 11:08:18 +0530

    Add files via upload

---
+++ Assignment_09/Assignment9_3.py
@@ -0,0 +1,11 @@
+# Write a program which accept one number and prints square of that number
+
+def Square():
+    No = int(input("enter number : "))
+    print(No**2)
+
+def main():
+    Square()
+
+if __name__=="__main__":
+    main()
---
+++ Assignment_09/Assignment9_4.py
@@ -0,0 +1,11 @@
+# Write a program which accept one number and prints cube of that number
+
+def Cube():
+    No = int(input("enter number : "))
+    print(No**3)
+
+def main():
+    Cube()
+
+if __name__=="__main__":
+    main()
